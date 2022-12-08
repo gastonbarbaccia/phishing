494cd92541a687bf6df750a2778936c6a91085c9
gastonbarbaccia <gastonbarbaccia@hotmail.com>
2022-12-08 17:19:03 -0300

    Se modifica el codigo de footer

--- campaing_details.php
+++ campaing_details.php
@@ -146,11 +146,24 @@
             </tbody>
         </table>
     </div>
-    <?php
 
-    include 'layouts/footer.php';
+    <style>
+        footer {
 
-    ?>
+            position: absolute;
+            width: 100%;
+
+        }
+    </style>
+
+    <footer class="bg-light text-center text-lg-start">
+        <!-- Copyright -->
+        <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+            © 2022 Copyright:
+            <a class="text-dark" href="#">Cencosud</a>
+        </div>
+        <!-- Copyright -->
+    </footer>
 
     <!-- JavaScript Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>

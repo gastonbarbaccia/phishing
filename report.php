5d1f5176e41953a18acfb6cbcc6e92a285eb3a34
gastonbarbaccia <gastonbarbaccia@hotmail.com>
2022-12-25 19:34:03 -0300

    Se realiza una mejora el menu para que sea dinamico

--- report.php
+++ report.php
@@ -93,25 +93,10 @@ $user_count = $stmt->fetchColumn();
 
 <body>
 
-    <nav class="navbar navbar-expand-lg bg-light">
-        <div class="container-fluid">
-            <a class="navbar-brand" href="index.php"><img src="assets/img/logo.png" width="100px" height="50px"> Phishing Platform</a>
-            <button id="navbartoggler" class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNavAltMarkup" name="navbarNavAltMarkup">
-                <div class="navbar-nav">
-                    <a class="nav-link" href="http://localhost/phishingBE/index.php">Dashboard</a>
-                    <a class="nav-link" href="http://localhost/phishingBE/campaing.php">Campaings</a>
-                    <a class="nav-link" href="http://localhost/phishingBE/users_groups.php">Users & Groups</a>
-                    <a class="nav-link" href="http://localhost/phishingBE/email_templates.php">Email template</a>
-                    <a class="nav-link" href="http://localhost/phishingBE/phishing_url_templates.php">Phishing URL</a>
-                    <button class="btn btn-secondary" style="margin-left: 200px;" id="button_print"><i class='fa fa-print' aria-hidden='true' style='font-size:20px;padding-right:10px'></i> Print report</button>
-                </div>
-            </div>
-        </div>
-    </nav>
+    <?php
 
+    include 'layouts/nav_report.php';
+?>
 
 
     <br>

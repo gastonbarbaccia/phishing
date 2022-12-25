072a53246bc7b698c769dab037fbc9aa7b9c7dcd
gastonbarbaccia <gastonbarbaccia@hotmail.com>
2022-12-25 19:28:44 -0300

    Se configura que las URL sean dinamicas para funcionar correctamente en servidores publicos

--- layouts/nav.php
+++ layouts/nav.php
@@ -1,3 +1,9 @@
+<?php
+
+$url = "http://ec2-18-117-162-122.us-east-2.compute.amazonaws.com/";
+
+?>
+
 <nav class="navbar navbar-expand-lg bg-light">
   <div class="container-fluid">
     <a class="navbar-brand" href="index.php"><img src="assets/img/logo.png" width="100px" height="50px"> Phishing Platform</a>
@@ -6,11 +12,11 @@
     </button>
     <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
       <div class="navbar-nav">
-        <a class="nav-link" href="http://localhost/phishingBE/index.php">Dashboard</a>
-        <a class="nav-link" href="http://localhost/phishingBE/campaing.php">Campaings</a>
-        <a class="nav-link" href="http://localhost/phishingBE/users_groups.php">Users & Groups</a>
-        <a class="nav-link" href="http://localhost/phishingBE/email_templates.php">Email template</a>
-        <a class="nav-link" href="http://localhost/phishingBE/phishing_url_templates.php">Phishing URL</a>
+        <a class="nav-link" href="<?php echo $url.'phishingBE/index.php'?>">Dashboard</a>
+        <a class="nav-link" href="<?php echo $url.'phishingBE/campaing.php'?>">Campaings</a>
+        <a class="nav-link" href="<?php echo $url.'phishingBE/users_groups.php'?>">Users & Groups</a>
+        <a class="nav-link" href="<?php echo $url.'phishingBE/email_templates.php'?>">Email template</a>
+        <a class="nav-link" href="<?php echo $url.'phishingBE/phishing_url_templates.php'?>">Phishing URL</a>
       </div>
     </div>
   </div>
